e0ebfc50dae97b74ea92d2ee5ad7f271b07894bf
SABINO KARI B <202100977b@utea.edu.pe>
2023-09-22 21:05:46 -0500

    todolist

--- listatareas.php
+++ listatareas.php
@@ -3,7 +3,8 @@ include('conexion.php');
 $query="SELECT id,tarea,completada FROM todolist.tareas order by id desc;";
 $rs = mysqli_query($cn,$query);
 $html="";
-while ($row = mysqli_fetch_assoc($rs)) { 
+$resultados = array();
+/*while ($row = mysqli_fetch_assoc($rs)) { 
     $html= $html."<tr>";
     $html= $html."<td>". $row["id"] . "</td>";
     $html= $html."<td>". $row["tarea"] . "</td>";
@@ -12,4 +13,11 @@ while ($row = mysqli_fetch_assoc($rs)) {
 
 }
 echo $html;
+*/
+while ($fila = mysqli_fetch_assoc($rs)) {
+    $resultados[] = $fila;
+}
+
+$json = json_encode($resultados); 
+echo $json;
 ?>
